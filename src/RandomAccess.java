94ce7db0bcf6776f599851b1305f056f0c5c2f96
Anton Österberg <anton.osterberg@gmail.com>
2016-12-02 23:14:04 +0100

    Updated Java Docs for the interface.

--- src/RandomAccess.java
+++ src/RandomAccess.java
@@ -1,9 +1,13 @@
+import java.util.List;
+import java.util.Set;
 
 /**
  * All data structures that implements the {@link Iterable} interface allows the
  * retrieval of a random element in the data structure. For example;
+ *
  * <pre>
- * {@code
+ * {
+ * 	&#64;code
  * 	final SecureRandom random = new SecureRandom();
  * 	final int elementIndex = random.nextInt(setOfElements.size());
  * 	int i = 0;
@@ -16,13 +20,22 @@
  * 	}
  * }
  * </pre>
- * would retrieve a random element from the collection <code>setOfElements</code>, but it would
- * be done with a time complexity of <code>O(n)</code>.
- * 
- * This interface is for data structures that supports retrieval of a random element
- * within the data structure by a dedicated method. This is preferably implemented with a time
- * complexity of <code>O(log n)</code> or <code>O(1)</code> as opposed to using
- * an iterator for random access of data element as with the example above.
+ *
+ * would retrieve a random element from the collection
+ * <code>setOfElements</code>, but it would be done with a time complexity of
+ * <code>O(n)</code>.
+ *
+ * This interface is for data structures that supports retrieval of a random
+ * element within the data structure by a dedicated method. This is preferably
+ * implemented with a time complexity of <code>O(log n)</code> or
+ * <code>O(1)</code> as opposed to using an iterator for random access of data
+ * element as with the example above.
+ *
+ * Unlike the default {@link java.util.RandomAccess} found in the Java Standard
+ * Library, this interface can be applied to for examples a {@link Set} and not
+ * only a {@link List} implementations. Additionally, this interface has one
+ * declared method, unlike the previously mentioned interface which is only a
+ * maker interface.
  * 
  * @author Anton &Ouml;sterberg
  *
